a4b8aacadc8ecb73092d70613b7f7e9747a5e681
vicduraes <duraes.vic@gmail.com>
2020-06-07 20:24:52 -0300

    Consumir API de contatos

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,29 @@ import Contacts from "./components/Contacts/Contacts";
 import "./assets/stylesheets/main.scss";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+    };
+  }
+
+  getAPI() {
+    fetch("https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts")
+      .then((response) => response.json())
+      .then((data) => this.setState({ contacts: data }));
+  }
+
+  componentDidMount() {
+    this.getAPI();
+  }
+
   render() {
     return (
       <>
         <Topbar />
         <Filters />
-        <Contacts />
+        <Contacts contacts={this.state.contacts} />
       </>
     );
   }
